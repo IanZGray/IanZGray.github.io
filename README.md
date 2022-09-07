ba635ce9b03b537566c03bc003f8f9de8adcb3b6
IanZGray <ianzacharygray@gmail.com>
2022-09-06 22:53:51 -0400

    readme

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ This githup page is designed to introduce myself as a web developer while provid
 
 Simply download the zip folder with the source code file, or fork it into your github and clone it into your machine, and run it by using your favorite IDE.
 
-Link to GitHub Deployment: **[Ian Gray Portfolio](https://ianzgray.github.io/).**
+Link to GitHub Deployment: **[Ian Gray Portfolio](https://ianzgray.netlify.app/).**
 
 ### Potential Future Changes
 
--- src/pages/work/Work.jsx
+++ src/pages/work/Work.jsx
@@ -20,14 +20,14 @@ function work() {
               <h1 className='work-header'>Gray Design</h1>
               <h3 className='work-sub-header'>Branding & Web Design</h3>
               <p className='work-p'>My small design business where you can find examples of my work in the UI branch of web development.</p>
-              <img src={design} alt="AWS Sustainability Project"  className="work-pics"/>
+              <img src={design} alt="Gray Design"  className="work-pics"/>
               <a href="https://www.designwithgray.com/" target="_blank" rel="noreferrer"><button className="work-button" alt="link to design page">+ More</button></a>
           </Col>
           <Col className='middle-box' xs={{span: 10, offset: 1}}>
               <h1 className='work-header'>Github</h1>
               <h3 className='work-sub-header'>All Currently Listed Repos</h3>
               <p className='work-p'>If you view examples of my code here, we can talk about it!</p>
-              <img src={git} alt="AWS Sustainability Project"  className="work-pics"/>
+              <img src={git} alt="GitHub"  className="work-pics"/>
               <a href="https://github.com/IanZGray" target="_blank" rel="noreferrer"><button className="work-button" alt="link to github">+ More</button></a>
           </Col>
           <Col className='side-box' xs={{span: 10, offset: 1}}>
@@ -35,7 +35,7 @@ function work() {
               <h1 className='work-header'>Books</h1>
               <h3 className='work-sub-header'>Fiction & Non-Fiction</h3>
               <p className='work-p'>Conveying information to others; whether through visuals, story, or under the hood code, is important to me. Browse my small library of works.</p>
-              <img src={books} alt="AWS Sustainability Project"  className="work-pics"/>
+              <img src={books} alt="books"  className="work-pics"/>
               <a href="https://www.amazon.com/~/e/B09NZ7DCS4" target="_blank" rel="noreferrer"><button className="work-button" alt="link to books">+ More</button></a>
               </div>
           </Col>
@@ -48,14 +48,14 @@ function work() {
             <h3 className='work-sub-header'>Branding & Web Design</h3>
             <p className='work-p'>My small design business where you can find examples of my work in the UI branch of web development.</p>
             <a href="https://www.designwithgray.com/" target="_blank" rel="noreferrer"><button className="work-button" alt="link to design page">+ More</button></a>
-            <img src={design} alt="AWS Sustainability Project"  className="work-pics"/>
+            <img src={design} alt="Gray Design"  className="work-pics"/>
         </Col>
         <Col className='middle-box' xs={{span: 4}}>
             <h1 className='work-header'>Github</h1>
             <h3 className='work-sub-header'>All Currently Listed Repos</h3>
             <p className='work-p'>If you view examples of my code here, we can talk about it!</p>
             <a href="https://github.com/IanZGray" target="_blank" rel="noreferrer"><button className="work-button" alt="link to github">+ More</button></a>
-            <img src={git} alt="AWS Sustainability Project"  className="work-pics"/>
+            <img src={git} alt="GitHub"  className="work-pics"/>
         </Col>
         <Col className='side-box' xs={{span: 4}}>
             <div className='cont'>
@@ -63,7 +63,7 @@ function work() {
             <h3 className='work-sub-header'>Fiction & Non-Fiction</h3>
             <p className='work-p'>Conveying information to others (whether through visuals, story, or under-the-hood code) is important to me. Browse my small library of works.</p>
             <a href="https://www.amazon.com/kindle-dbs/entity/author/B09NZ7DCS4?_encoding=UTF8&node=283155&offset=0&pageSize=12&searchAlias=stripbooks&sort=author-sidecar-rank&page=1&langFilter=default" target="_blank" rel="noreferrer"><button className="work-button" alt="link to books">+ More</button></a>
-            <img src={books} alt="AWS Sustainability Project"  className="work-pics"/>
+            <img src={books} alt="Books"  className="work-pics"/>
             </div>
         </Col>
     </Row>
