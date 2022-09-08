1aabd5fa4e38e0ed6c0430f30a15780ec78ad545
IanZGray <ianzacharygray@gmail.com>
2022-09-07 22:56:31 -0400

    fixed resume div

--- src/components/resumeButton/ResumeButton.jsx
+++ src/components/resumeButton/ResumeButton.jsx
@@ -5,10 +5,8 @@ import './resumeButton.css'
 import pdf from './Ian_Gray_Resume_2022.pdf'
 function ResumeButton() {
   return (
-    <div className='resume-button'>
+    <div >
           <a href={pdf} without rel='noopener noreferrer' target="_blank" ><button className="resume-header" alt="resume">Resume</button></a>
-
-        <div>{pdf}</div>
     </div>
   )
 }
