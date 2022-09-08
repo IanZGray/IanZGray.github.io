72303faa4554aa0b3bc701bd10ff09b1702180ad
IanZGray <ianzacharygray@gmail.com>
2022-09-07 22:07:02 -0400

    uploaded resume

--- src/components/resumeButton/ResumeButton.jsx
+++ src/components/resumeButton/ResumeButton.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './resumeButton.css'
 
+import pdf from './Ian_Gray_Resume_2022.pdf'
 function ResumeButton() {
-
   return (
     <div className='resume-button'>
-          <button className="resume-header" alt="resume">Resume</button>
+          <a href={pdf} without rel='noopener noreferrer' target="_blank" ><button className="resume-header" alt="resume">Resume</button></a>
+
+        <div>{pdf}</div>
     </div>
   )
 }
